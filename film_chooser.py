2b4dc880140d53cabce9dd179459a31369345714
Alina Muliak <alina.muliak@ucu.edu.ua>
2021-02-12 23:19:50 +0200

    Create film_chooser.py module and a small.list file

---
+++ film_chooser.py
@@ -0,0 +1,12 @@
+def read_file(file: str) -> list:
+    """
+    Read data from file and return the list.
+    """
+    pass
+
+
+def one_year_films(year: int, film_list: list) -> list:
+    """
+    повертати фільми заданого року
+    """
+    pass
