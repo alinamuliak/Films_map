6aea669b5b97e2324fa27d2b4fabd1d7a2e2f342
Alina Muliak <alina.muliak@ucu.edu.ua>
2021-02-16 16:54:17 +0200

    Update film_chooser.py, main.py and small.list

--- film_chooser.py
+++ film_chooser.py
@@ -1,4 +1,5 @@
 import folium
+from folium.plugins import MarkerCluster
 from copy import deepcopy
 from haversine import haversine
 from geopy.geocoders import Nominatim
@@ -57,25 +58,16 @@ def address_to_coordinates(film_list: list) -> list:
     coord_list = []
     for film_info in film_list:
         try:
-            # for film_info in coord_list:
-            # print(film_info[-1], flush=True)
             location = geolocator.geocode(film_info[-1])
-            # print("location =", location, flush=True)
             coord_list.append([
                 film_info[0], (location.latitude, location.longitude)])
-            print("ok", (location.latitude, location.longitude), flush=True)
+            print("ok", len(coord_list),
+                  (location.latitude, location.longitude), flush=True)
         except GeocoderUnavailable:
-            # print("geoexc", film_info, flush=True)
             continue
         except AttributeError:
-            # print("attr", film_info, flush=True)
             continue
     return coord_list
-
-
-# coord_list = address_to_coordinates(one_year)
-# print(coord_list, flush=True)
-# print("address to coord", flush=True)
 
 
 def determine_ten_closest(film_list: list, user_location: tuple) -> list:
@@ -85,7 +77,6 @@ def determine_ten_closest(film_list: list, user_location: tuple) -> list:
     closest_locations = []
     for film_info in film_list:
         film_location = film_info[-1]
-        # print((user_location, film_location), flush=True)
         distance = haversine(user_location, film_location)
         if len(closest_locations) == 0:
             closest_locations.append((film_info[0], distance, film_location))
@@ -97,16 +88,10 @@ def determine_ten_closest(film_list: list, user_location: tuple) -> list:
               distance < closest_locations[-1][1]):
             closest_locations[-1] = (film_info[0], distance, film_location)
             closest_locations.sort(key=lambda x: x[1])
-    return closest_locations
+    return closest_locations[:10]
 
 
-# coord_list = [['"#ActorsLife"', (40.7127281, -74.0060152)], ['"#Fuga"', (-22.9997404, -43.3659929)], ['"#KillTorrey"', (34.1816482, -118.3258554)], ['"#LoveMyRoomie"', (40.7127281, -74.0060152)], ['"#LoveMyRoomie"', (40.6501038, -73.9495823)], ['"#MyCurrentSituation: Atlanta"', (33.7489924, -84.3902644)], ['"#MyCurrentSituation: Atlanta"', (35.1490215, -90.0516285)], ['"#SmurTv"', (37.270973, -79.9414313)], ['"#SmurTv"', (35.2272086, -80.8430827)], ['"#SmurTv"', (33.7489924, -84.3902644)], ['"#SpongeyLeaks"', (34.2345615, -118.5369316)], ['"#SpongeyLeaks"', (34.2345615, -118.5369316)], ['"#VanLifeAttila"', (61.0666922, -107.991707)], ['"#VanLifeAttila"', (36.7014631, -118.755997)], ['"#VanLifeAttila"', (51.0, 10.0)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0962821, -121.5323561)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0962821, -121.5323561)], ['"#VanLifeAttila"', (49.2433804, -122.9725459)], ['"#VanLifeAttila"', (49.2842958, -122.793281)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (52.9794279, -122.4936273)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (49.3206294, -123.0737925)], ['"#VanLifeAttila"', (49.3206294, -123.0737925)], ['"#VanLifeAttila"', (49.3206294, -123.0737925)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (52.9794279, -122.4936273)], ['"#VanLifeAttila"', (49.8879177, -119.4959025)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (49.2842958, -122.793281)], ['"#VanLifeAttila"', (49.2842958, -122.793281)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)],
-#               ['"#VanLifeAttila"', (49.163168, -123.137414)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (53.0962821, -121.5323561)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (50.8367991, -118.9826386)], ['"#VanLifeAttila"', (51.0534234, -114.0625892)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (49.3799438, -121.4413515)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (50.8367991, -118.9826386)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (49.2433804, -122.9725459)], ['"#VanLifeAttila"', (49.3206294, -123.0737925)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (49.3019112, -123.14154052837662)], ['"#VanLifeAttila"', (55.001251, -115.002136)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (47.4983815, 19.0404707)], ['"#VanLifeAttila"', (47.4983815, 19.0404707)], ['"#VanLifeAttila"', (53.0962821, -121.5323561)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (49.2842958, -122.793281)], ['"#VanLifeAttila"', (49.3206294, -123.0737925)], ['"#VanLifeAttila"', (49.2608724, -123.1139529)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (53.0402309, -121.718755)], ['"#VanLifeAttila"', (52.9794279, -122.4936273)], ['"#VanLifeAttila"', (53.0666687, -121.5166749)], ['"#VanLifeAttila"', (53.1044428, -121.5723679)], ['"#VanLifeAttila"', (53.1173414, -121.49901337494916)]]
-# closest_films = determine_ten_closest(coord_list, (40.730610, -73.935242))
-# print("closest 10 - done", flush=True)
-
-
-def map_builder(film_list: list, user_location: tuple):
+def map_builder(film_list: list, user_location: tuple, user_year):
     """
     """
     map = folium.Map(location=list(user_location))
@@ -114,13 +99,20 @@ def map_builder(film_list: list, user_location: tuple):
     lon = [x[-1][1] for x in film_list]
     names = [x[0] for x in film_list]
     flm = folium.FeatureGroup(name="Films Marker")
+    lines_loc = folium.FeatureGroup(name="Connected with you")
+    marker_cluster = MarkerCluster().add_to(flm)
+
     for lt, ln, nm in zip(lat, lon, names):
-        flm.add_child(folium.Marker(location=[lt, ln],
-                                    popup=nm,
-                                    icon=folium.Icon()))
+        folium.Marker(location=[lt, ln],
+                      popup=nm,
+                      icon=folium.Icon(color='red')).add_to(marker_cluster)
+        folium.PolyLine(
+            locations=[list(user_location), [lt, ln]], weight=2).add_to(lines_loc)
+
     map.add_child(flm)
-    map.save('film_map.html')
+    map.add_child(lines_loc)
+    folium.LayerControl().add_to(map)
+    map.save(f'{user_year}_film_map.html')
 
 
 # map_builder(closest_films, (40.730610, -73.935242))
-# print("map - done")
--- main.py
+++ main.py
@@ -17,14 +17,15 @@ def main():
     user_location = (float(user_location_str.split(',')[0][1:]),
                      float(user_location_str.split(',')[1][:-1]))
     slow("Map is generating...\n")
-    # TRY WITH LOCATION.LIST !!!&!^$%#@#$%#@!@#$%
-    film_list = flmc.read_file("locations.list")
+
+    film_list = flmc.read_file("small.list")
     one_year = flmc.one_year_films(year, film_list)
     coord_list = flmc.address_to_coordinates(one_year)
     slow("Please wait...\n")
     closest_films = flmc.determine_ten_closest(coord_list, user_location)
-    flmc.map_builder(closest_films, user_location)
-    slow("Finished. Please have look at the map movies_map.html\n")
+    flmc.map_builder(closest_films, user_location, year)
+    slow(
+        f"Finished. Please have a look at the {year}_film_map.html\n")
 
 
 if __name__ == "__main__":
